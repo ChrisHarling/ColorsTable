fdd138a9eda0151827f890645b07cb6de36b9b40
Chris Harling <cehxc5@mail.missouri.edu>
2019-03-08 13:19:27 -0600

    Added table list of colors and added the correct background color for the colors listed

---
+++ Colors/Colors/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Colors
+//
+//  Created by Chris Harling on 3/8/19.
+//  Copyright © 2019 Chris Harling. All rights reserved.
+//
+
+import UIKit
+
+struct Color{
+    let name: String
+    let uiColor: UIColor
+}
+
